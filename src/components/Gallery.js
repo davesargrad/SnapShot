6aa3619a9421a664208a0edcc0890b9046d37bf7
David Sargrad <48793689+davesargrad@users.noreply.github.com>
2020-10-02 10:15:37 -0400

    introduced the use of a gridlist into the gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,29 @@
 import React from "react";
 import NoImages from "./NoImages";
 import Image from "./Image";
+
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 1200,
+      height: 450,
+    },
+  }),
+);
+
 const Gallery = props => {
+  const classes = useStyles();
   const results = props.data;
   let images;
   let noImages;
@@ -14,15 +36,17 @@ const Gallery = props => {
       let secret = image.secret;
       let title = image.title;
       let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
-      return <Image url={url} key={id} alt={title} />;
+      return <GridListTile key={id} cols={image.cols || 1}><Image url={url}  alt={title} /></GridListTile>;
     });
   } else {
     noImages = <NoImages />; // return 'not found' component if no images fetched
   }
   return (
-    <div>
-      <ul>{images}</ul>
-      {noImages}
+    <div className={classes.root}>
+      <GridList cellHeight={'auto'} className={classes.gridList} cols={5} spacing={40} >
+        {images}
+        {noImages}
+      </GridList>
     </div>
   );
 };
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,16 +2,16 @@ import React from "react";
 import Container from "./Container";
 
 import Typography from '@material-ui/core/Typography';
-
+import Box from '@material-ui/core/Box';
 const Item = ({ searchTerm }) => {
   return (
-    <div>
+    <Box component="div" m={1}>
       <Typography variant="h2" gutterBottom>
         {searchTerm} Pictures
       </Typography>
 
       <Container searchTerm={searchTerm} />
-    </div>
+    </Box>
   );
 };
 
